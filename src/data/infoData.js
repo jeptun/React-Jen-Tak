970259637301da9a57b79ba4f9dcdcb95e2115f8
jeptun <73310547+jeptun@users.noreply.github.com>
2021-10-10 15:24:19 +0200

    Update infoData.js

--- src/data/infoData.js
+++ src/data/infoData.js
@@ -4,8 +4,8 @@ import ImageEight from '../img/img1.jpg';
 
 export const InfoData ={
     heading: 'Prohledni si co budes chtit',
-    paragraphOne: 'Learn how to make a React website with animations. This React JS project is completely responsive and focuses on a real estate design. ', 
-    paragraphTwo:  'm back with the Awwwards Rebuilt Episode 2 and this site is AWESOME. I appreciate how patient you guys have been with me and hope everyone is doing well in these tough times. Look out for part 2 tomorrow ' ,   
+    paragraphOne: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum consectetur minima, deleniti perspiciatis in cum nam reiciendis quod nobis iure. Enim dolor laboriosam pariatur, quibusdam tenetur voluptate quod hic? ', 
+    paragraphTwo:  ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum consectetur minima, deleniti perspiciatis in cum nam reiciendis quod nobis iure. Enim dolor laboriosam pariatur, quibusdam tenetur voluptate quod hic? ' ,   
     buttonLabel:'View Book',
     image: ImageEight,
     reverse:false,
